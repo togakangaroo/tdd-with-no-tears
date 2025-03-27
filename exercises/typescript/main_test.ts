99e6e03704e075cbf010b75cafa23e3f40817a3b
George Mauer <gmauer@gmail.com>
2025-03-27 09:12:03 -0500

    passing tests with time

--- exercises/typescript/main_test.ts
+++ exercises/typescript/main_test.ts
@@ -1,4 +1,5 @@
-import { describe, it, before } from "jsr:@std/testing/bdd"
+import { describe, it, before, after } from "jsr:@std/testing/bdd"
+import { FakeTime } from 'https://deno.land/std/testing/time.ts'
 import { assertEquals } from "@std/assert";
 
 type StopWatch = {
@@ -7,20 +8,28 @@ type StopWatch = {
 }
 
 const createStopWatch = (): StopWatch  => {
-    let displayState: number | null = null
+    let startTime: number | null = null
     return {
-        start: () => displayState = 0,
-        display: () => displayState,
+        start: () => startTime = Date.now(),
+        display: () => !startTime ? null :(Date.now() - startTime) / 1000 
     }
 }
 
 describe(`Given a new stopwatch with a main and lap slot`, () => {
     let sw: StopWatch = null as any
+    let time : FakeTime = null as any
+    before(() => { time = new FakeTime() })
+    after(() => {time[Symbol.dispose]()})
     before(() => {sw = createStopWatch()})
 
     it(`reads empty`, () => assertEquals(sw.display(), null))
     describe(`When timer started`, () => {
         before(() => { sw.start() })
         it(`reads 0`, () => assertEquals(sw.display(), 0))
+
+        describe(`When 10s have passed`, () => {
+            before(() => time.tick(10*1000))
+            it(`displays 10s`, () => assertEquals(sw.display(), 10))
+        })
     })
 })
