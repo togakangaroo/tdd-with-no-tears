08f977262cdd1398a8a3832aba5a9eb7402cabd7
George Mauer <gmauer@gmail.com>
2025-03-26 23:23:03 -0500

    some basic test failures

--- exercises/typescript/main_test.ts
+++ exercises/typescript/main_test.ts
@@ -1,10 +1,22 @@
-import { describe, it, beforeEach } from "jsr:@std/testing/bdd"
+import { describe, it, before } from "jsr:@std/testing/bdd"
 import { assertEquals } from "@std/assert";
 
-describe(`Given a  new timer with a main and lap slot`, () => {
+type StopWatch = {
+    display: () => number | null
+}
 
-    it(`reads empty`, () => {})
+const createStopWatch = (): StopWatch  => {
+    return {
+        display: () => null
+    }
+}
+
+describe(`Given a new stopwatch with a main and lap slot`, () => {
+    let sw: StopWatch = null as any
+    before(() => {sw = createStopWatch()})
+
+    it(`reads empty`, () => assertEquals(sw.display(), null))
     describe(`When timer started`, () => {
-        it(`reads 0`, () => {})
+        it(`reads 0`, () => assertEquals(sw.display(), 0))
     })
 })
