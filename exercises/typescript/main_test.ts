10c229ecca4e0f04a0440b6ebbd641445a3dc1b0
George Mauer <gmauer@gmail.com>
2025-03-26 23:11:58 -0500

    initial project creation and setup

---
+++ exercises/typescript/main_test.ts
@@ -0,0 +1,10 @@
+import { describe, it, beforeEach } from "jsr:@std/testing/bdd"
+import { assertEquals } from "@std/assert";
+
+describe(`Given a  new timer with a main and lap slot`, () => {
+
+    it(`reads empty`, () => {})
+    describe(`When timer started`, () => {
+        it(`reads 0`, () => {})
+    })
+})
