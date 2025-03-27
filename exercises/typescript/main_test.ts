1c89da36dd350c3c524b7a2d8368e7a984b069e0
George Mauer <gmauer@gmail.com>
2025-03-26 23:26:12 -0500

    first few passing tests

--- exercises/typescript/main_test.ts
+++ exercises/typescript/main_test.ts
@@ -2,12 +2,15 @@ import { describe, it, before } from "jsr:@std/testing/bdd"
 import { assertEquals } from "@std/assert";
 
 type StopWatch = {
+    start: () => unknown
     display: () => number | null
 }
 
 const createStopWatch = (): StopWatch  => {
+    let displayState: number | null = null
     return {
-        display: () => null
+        start: () => displayState = 0,
+        display: () => displayState,
     }
 }
 
@@ -17,6 +20,7 @@ describe(`Given a new stopwatch with a main and lap slot`, () => {
 
     it(`reads empty`, () => assertEquals(sw.display(), null))
     describe(`When timer started`, () => {
+        before(() => { sw.start() })
         it(`reads 0`, () => assertEquals(sw.display(), 0))
     })
 })
