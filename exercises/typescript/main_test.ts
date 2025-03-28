2438c54a46bfd9afbdc6d9a89f5d9bce83f55c29
George Mauer <gmauer@gmail.com>
2025-03-28 00:13:43 -0500

    occasion for further refactoring

--- exercises/typescript/main_test.ts
+++ exercises/typescript/main_test.ts
@@ -1,19 +1,24 @@
-import { describe, it, before, after } from "jsr:@std/testing/bdd"
+import { describe, it, beforeEach, afterEach } from "jsr:@std/testing/bdd"
 import { FakeTime } from 'https://deno.land/std/testing/time.ts'
 import { assertEquals } from "@std/assert";
 
 type StopWatch = {
-    start: () => unknown
+    start: () => void
     display: () => number | null
     laps: () => number[]
+    lap: () => void
 }
+
+const secSince = (startTime: number) => (Date.now() - startTime) / 1000
 
 const createStopWatch = (): StopWatch  => {
     let startTime: number | null = null
+    let laps: number[] = []
     return {
         start: () => startTime = Date.now(),
-        display: () => !startTime ? null :(Date.now() - startTime) / 1000,
-        laps: () => []
+        display: () => !startTime ? null : secSince(startTime),
+        laps: () => laps,
+        lap: () => laps = !startTime ? [] : [...laps, secSince(startTime)],
     }
 }
 
@@ -21,22 +26,36 @@ const createStopWatch = (): StopWatch  => {
 describe(`Given a new stopwatch with a main and lap slot`, () => {
     let sw: StopWatch = null as any
     let time : FakeTime = null as any
-    before(() => { time = new FakeTime() })
-    after(() => {time?.[Symbol.dispose]()})
-    before(() => {sw = createStopWatch()})
+    beforeEach(() => { time = new FakeTime() })
+    afterEach(() => {time?.[Symbol.dispose]()})
+    beforeEach(() => {sw = createStopWatch()})
 
     const displayShouldRead = (sec: number) =>
         it(`displays ${sec}s`, () => assertEquals(sw.display(), sec))
 
     it(`reads empty`, () => assertEquals(sw.display(), null))
-    describe(`When timer started`, () => {
-        before(() => { sw.start() })
+
+    describe(`when timer started`, () => {
+        beforeEach(() => { sw.start() })
         displayShouldRead(0)
 
-        describe(`When 10s have passed`, () => {
-            before(() => time.tick(10*1000))
+        describe(`when 10s have passed`, () => {
+            beforeEach(() => time.tick(10 * 1000))
             displayShouldRead(10)
             it(`There are no laps recorded`, () => assertEquals(sw.laps(), []))
+
+            describe(`when 1s has passed`, () => {
+                beforeEach(() => time.tick(1*1000))
+                displayShouldRead(11)
+                it(`There are no laps recorded`, () => assertEquals(sw.laps(), []))
+            })
+
+            describe(`when lap hit`, () => {
+                beforeEach(() => sw.lap())
+                displayShouldRead(10)
+                it(`Shows laps of 10s`, () => assertEquals(sw.laps(), [10]))
+            })
+
         })
     })
 })
