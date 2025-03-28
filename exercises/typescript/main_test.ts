ce74e3672ab8945ddd096d32b5245a223e8b0e6e
George Mauer <gmauer@gmail.com>
2025-03-27 23:59:15 -0500

    test laps and first refacotring

--- exercises/typescript/main_test.ts
+++ exercises/typescript/main_test.ts
@@ -5,31 +5,38 @@ import { assertEquals } from "@std/assert";
 type StopWatch = {
     start: () => unknown
     display: () => number | null
+    laps: () => number[]
 }
 
 const createStopWatch = (): StopWatch  => {
     let startTime: number | null = null
     return {
         start: () => startTime = Date.now(),
-        display: () => !startTime ? null :(Date.now() - startTime) / 1000 
+        display: () => !startTime ? null :(Date.now() - startTime) / 1000,
+        laps: () => []
     }
 }
+
 
 describe(`Given a new stopwatch with a main and lap slot`, () => {
     let sw: StopWatch = null as any
     let time : FakeTime = null as any
     before(() => { time = new FakeTime() })
-    after(() => {time[Symbol.dispose]()})
+    after(() => {time?.[Symbol.dispose]()})
     before(() => {sw = createStopWatch()})
+
+    const displayShouldRead = (sec: number) =>
+        it(`displays ${sec}s`, () => assertEquals(sw.display(), sec))
 
     it(`reads empty`, () => assertEquals(sw.display(), null))
     describe(`When timer started`, () => {
         before(() => { sw.start() })
-        it(`reads 0`, () => assertEquals(sw.display(), 0))
+        displayShouldRead(0)
 
         describe(`When 10s have passed`, () => {
             before(() => time.tick(10*1000))
-            it(`displays 10s`, () => assertEquals(sw.display(), 10))
+            displayShouldRead(10)
+            it(`There are no laps recorded`, () => assertEquals(sw.laps(), []))
         })
     })
 })
